61ed7faaa9d3431c06947a1f83fe739753cb0c23
MonsterLaplace <104540426+MonsterLaplace@users.noreply.github.com>
2026-07-14 10:40:29 +0800

    Update 03.ROH detection and genomic inbreeding.sh

--- WGS downstream analysis/03.ROH detection and genomic inbreeding.sh
+++ WGS downstream analysis/03.ROH detection and genomic inbreeding.sh
@@ -24,6 +24,8 @@ plink --bfile all_samples.nomaf \
 ##################################
 #2. 后续统计处理以及inbreeding计算
 ##################################
+python stat_roh_by_sample.py
+python stat_roh_count_and_length.py
 python make_roh_window_frequency.py -r  all_samples.roh.hom -f  FMdeer.primary.fa.fai -o roh_window_frequency.tsv
 python calc_roh_window_frequency.py
 Rscript plot_roh_final.R
